9239d8e34609a219a76a2e29f76313301fdd8dce
Chris Van Patten <chris@vanpattenmedia.com>
2012-04-13 10:58:03 -0400

    reorganized, added more slides

---
+++ config.rb
@@ -0,0 +1,18 @@
+# Require
+require 'rgbapng'
+
+# Config
+css_dir			= "css"
+sass_dir		= "sass"
+images_dir		= "img"
+
+# Development
+# output_style	= :expanded
+# line_comments	= true
+
+# Production
+line_comments	= false
+output_style	= :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
