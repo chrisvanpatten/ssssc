cbb860a6b0a699fccb7de663db1057b25dfea7d2
Chris Van Patten <chris@vanpattenmedia.com>
2012-04-13 16:35:47 -0400

    slide changes

--- config.rb
+++ config.rb
@@ -7,12 +7,12 @@ sass_dir		= "sass"
 images_dir		= "img"
 
 # Development
-# output_style	= :expanded
-# line_comments	= true
+output_style	= :expanded
+#line_comments	= true
 
 # Production
 line_comments	= false
-output_style	= :compressed
+#output_style	= :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
